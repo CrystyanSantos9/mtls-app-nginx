a3daf0cc039d172b482fe8cc155dba68dfa0604f
CrystyanSantos9 <cryssantos9@gmail.com>
2023-04-19 09:22:40 -0300

    corrigindo mtls

--- api/server.js
+++ api/server.js
@@ -2,45 +2,77 @@
 // // // 'use strict';
 
 const express = require('express');
+const cors = require('cors')
 
 
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.NODE_NAME || '0.0.0.0';
 
+
+
 const app = express();
+
+// app.use(cors({
+//   origin: "*",
+// }))
+
+//middlewares
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// //cors
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// )
+
+
+
+// rotas 
+
 app.get('/', (req, res) => {
     const headers = req.headers
-  res.send(headers);
+    console.log("Headers from client >>> ", headers)
+    setTimeout(()=>
+
+     res.send(headers), 15*1000)
+    // res.destroy(), 9*1000)
 });
+
+//options por operação 
+
+app.options('/user', (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin","http://127.0.0.1:5501")
+  res.setHeader("Access-Control-Allow-Headers", "content-type")
+  res.setHeader("Access-Control-Allow-Methods","GET, OPTIONS, PUT, PATCH, DELETE")
+ 
+  res.status(204).send()
+});
+
+// corsOptions = {
+  
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+  
+// }
+// // app.options('/user', cors(corsOptions))
+
+app.put('/user', (req, res) => {
+  const { user } = req.body
+  const headers = req.headers
+  const newBody = {
+    user: user,
+    headers: headers
+  }
+  res.setHeader("Access-Control-Allow-Origin","*")
+
+  res.statusCode = 201
+  res.send(newBody);
+});
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-
-// -----------------------------------------------------------------
-
-// //HTTPS
-// const express = require('express');
-// const https = require('https');
-// const fs = require('fs')
-
-// const port = 8080;
-// const HOST = '0.0.0.0';
-
-// É preciso mapear essas pastas para o docker-compose
-// const options = {
-//     key: fs.readFileSync('./ssl/server.key'),
-//     cert: fs.readFileSync('./ssl/server.crt')
-//   };
-
-// // const options = {}
-
-// const app = express();
-
-// app.get('/', (req, res) => {
-//     res.status(200)
-//         .json(`Hello ${req.header("ssl_client")}, your certificate was issued by ${req.header("SSL_Client_Issuer")}!`);
-// });
-
-// https.createServer(options, app).listen(port, () => {
-//     console.log(`.. server up and running and listening on ${port} ..`);
-// });
